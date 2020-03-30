f78d586639b802b4c41de6b5be801c5bcb96fb5f
Ilya <chernyi.ilya.00@gmail.com>
2020-03-30 21:22:25 +0300

    lesson 2 hard done

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-let money = 50000;
-let income = "Freelance";
-let addExpences = "Internet connection, Water, Light";
-let deposit = true;
-let mission = 1000000;
-let period = 36;
-// alert('hi');
-// console.log(money);
-// console.log(2);
-console.log(typeof(money), typeof(income), typeof(deposit));
-console.log(addExpences.length);
-console.log(addExpences.toLowerCase().split(', '));
-let budgetDay = money / 30;
-console.log(budgetDay);
+let money = 266219;
+let money_str = money.toString();
+let mul = (money_str[0] - '0') * (money_str[1] - '0') * (money_str[2] - '0') * (money_str[3] - '0') * (money_str[4] - '0') * (money_str[5] - '0');
+console.log(mul);
+mul **=3;
+mul_str = mul.toString();
+console.log(mul_str[0]+mul_str[1]);
+
+// console.log();
