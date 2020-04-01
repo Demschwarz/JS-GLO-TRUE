a17e28c9e60a3252be4c52ccb41210862a8b4d32
Ilya <chernyi.ilya.00@gmail.com>
2020-04-01 16:05:03 +0300

    lesson_3_usual_rewrite

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 'use strict'
 let mission = 1000000;
-let money = '10000';
-let income = '50000';
-let deposit = true;
-console.log(typeof(money), typeof(income), typeof(deposit));
-// console.log(typeof(money));
-// let income = "Freelance";
+let income = '50000'; // всё это про прошлый урок, чтобы функционало всё, нужна переменная income, чтобы её тип потом определить
 let period = 6;
-let addExpences = ['Интернет', 'Коммуналка', 'Пропитание', 'Курсы по js'];
-console.log(addExpences.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель - заработать ' + mission + ' рублей');
-money = Number(prompt('Ваш месячный доход?', 36000));
-addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+let money = prompt('Ваш месячный доход?', '36000');
+let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
 console.log(addExpences.toLowerCase().split(', '));
-deposit = Boolean(prompt('Есть ли у вас депозит в банке?'));
-let expences1 = prompt('Введите обязательную статью расходов');
+console.log(addExpences.length);
+
+let deposit = !!(prompt('Есть ли у вас депозит в банке?'));
+
+console.log(typeof(money), typeof(income), typeof(deposit));
+
+money = +money;//не перевёл сразу, ибо нужно, чтобы выводился string как тип переменной money
+
+let expences1 = prompt('Введите обязательную статью расходов'); // вопросы к пользователю про статьи расхода
 let amount1 = Number(prompt('Во сколько это обойдется?'));
+
 let expences2 = prompt('Введите обязательную статью расходов');
 let amount2 = Number(prompt('Во сколько это обойдется?'));
+
 let budgetMonth = money - amount1 - amount2;
 console.log('Бюджет на месяц ' + budgetMonth);
+
 console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
+
 let budgetDay = budgetMonth / 30;
+
 console.log('Бюджет на день: ' + Math.round(budgetDay));
-if (budgetDay >= 1200){
+
+if (budgetDay >= 1200){ // вывод уровня дохода
     console.log('У вас высокий уровень дохода');
 }
 else if (budgetDay >= 600) {
@@ -36,14 +44,3 @@ else if (budgetDay < 0) {
 else {
     console.log('К сожалению у вас уровень дохода ниже среднего');
 }
-// // addExpences
-// // console.log('addExpences: ', addExpences);
-// // console.log(typeof(addExpences));
-// // let deposit = true;
-// let period = 36;
-// // // alert('hi');
-// // // console.log(money);
-// // // console.log(2);
-// console.log(addExpences.length);
-// console.log(addExpences.toLowerCase().split(', '));
-// console.log(budgetDay);
