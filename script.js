37aa67299e71c8513d6b0569a7c68c6aac5f1f10
Ilya <chernyi.ilya.00@gmail.com>
2020-04-03 15:26:07 +0300

    lesson 5 hard done

--- script.js
+++ script.js
@@ -1,103 +1,42 @@
-'use strict'
+// задание 1
+console.log('задание 1: ');
 
-let isNum = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+let isSuitble = function(n) {
+    let n_num = +n;
+    while (n_num > 10) {
+        n_num = Math.floor(n_num / 10);
+    }
+    return (n_num == 2) || (n_num == 4);
 }
 
-let showTypeOf = function (a) {
-    console.log(a + ' ' + typeof(a));
+let arr = [];
+arr = ['200', '324', '128', '477', '522', '674', '999'];
+for (let i = 0; i != 7; i++) {
+    if (isSuitble(arr[i])) {
+        console.log(arr[i]);
+    }
 }
 
-let mission = 1000000,
-    income = '50000';
+
+//задание 2
+console.log('задание 2: ');
 
 
-let money;
-
-let start = function() {
-    do {
-        money = +prompt('Ваш месячный доход?');
-    } while (!isNum(money))
-}
-
-start();
-
-let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpences.toLowerCase().split(', ')); // Вывод возможных расходов в виде массива 
-
-let deposit = confirm('Есть ли у вас депозит в банке?');
-
-
-console.log(typeof(money), typeof(income), typeof(deposit));
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-money = +money; //не перевёл сразу, ибо нужно, чтобы выводился string как тип переменной money
-
-//статьи расходов
-// let expences1 = prompt('Введите обязательную статью расходов'); // вопросы к пользователю про статьи расхода
-// let amount1 = Number(prompt('Во сколько это обойдется?'));
-
-// let expences2 = prompt('Введите обязательную статью расходов');
-// let amount2 = Number(prompt('Во сколько это обойдется?'));
-
-let expences = [];
-
-function getExpensesMonth() { 
-    let sum = 0;
-    for (let i = 0; i != 2; i++) {
-        expences[i] = prompt('Введите обязательную статью расходов');
-        let temp = +prompt('Во сколько это обойдётся?');
-        while (!isNum(temp)) { // проверка на число
-            temp = +prompt('Во сколько это обойдётся? Введите число');
+let isPrime = function(n) {
+    if ((n == 1) || (n == 2)) {
+        return true
+    }
+    for (let i = 2; i != Math.floor(n / 2); i++) {
+        if (n % i == 0) {
+            return false;
         }
-        sum += temp;
     }
-    return sum;
+    return true;
 }
 
-let expencesAmount = getExpensesMonth();
+for (let i = 1; i != 100; i++) {
+    if (isPrime(i)) {
+        console.log(i);
 
-console.log('getExpensesMonth(): ', expencesAmount);
-
-
-function getAccumulatedMonth() {
-    return money - expencesAmount;
-}
-
-
-let accumulatedMonth = getAccumulatedMonth();
-function getTargetMonth() {
-    let temp = Math.ceil(mission / accumulatedMonth);
-    if (temp <= 0) {
-        return 'Цель не будет достигнута';
-    }
-    return 'Цель будет достигнута за ' + temp + ' месяцев';
-}
-
-console.log(getTargetMonth());
-
-
-
-let budgetDay = Math.round(accumulatedMonth / 30);
-console.log('Бюджет на день: ' + budgetDay);
-
-
-function getStatusIncome() {
-    if (budgetDay >= 1200){ // вывод уровня дохода
-        console.log('У вас высокий уровень дохода');
-    }
-    else if (budgetDay >= 600) {
-        console.log('У вас средний уровень дохода');
-    }
-    else if (budgetDay < 0) {
-        console.log('Что-то пошло не так');
-    }
-    else {
-        console.log('К сожалению у вас уровень дохода ниже среднего');
     }
 }
-
-getStatusIncome(); // покказать степень дохода
