48b9320a83a90405a58b21c35136aedf89eb0364
Ilya <chernyi.ilya.00@gmail.com>
2020-04-03 12:27:42 +0300

    lesson 5 usual done

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 'use strict'
-let mission = 1000000;
-let income = '50000';
 
-let money = prompt('Ваш месячный доход?', '36000');
+let isNum = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+let showTypeOf = function (a) {
+    console.log(a + ' ' + typeof(a));
+}
+
+let mission = 1000000,
+    income = '50000';
+
+
+let money;
+
+let start = function() {
+    do {
+        money = +prompt('Ваш месячный доход?');
+    } while (!isNum(money))
+}
+
+start();
+
 let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-
 console.log(addExpences.toLowerCase().split(', ')); // Вывод возможных расходов в виде массива 
 
 let deposit = confirm('Есть ли у вас депозит в банке?');
 
-
-function showTypeOf(a) {
-    console.log(a + ' ' + typeof(a));
-}
 
 console.log(typeof(money), typeof(income), typeof(deposit));
 
@@ -23,30 +37,47 @@ showTypeOf(deposit);
 money = +money; //не перевёл сразу, ибо нужно, чтобы выводился string как тип переменной money
 
 //статьи расходов
-let expences1 = prompt('Введите обязательную статью расходов'); // вопросы к пользователю про статьи расхода
-let amount1 = Number(prompt('Во сколько это обойдется?'));
+// let expences1 = prompt('Введите обязательную статью расходов'); // вопросы к пользователю про статьи расхода
+// let amount1 = Number(prompt('Во сколько это обойдется?'));
 
-let expences2 = prompt('Введите обязательную статью расходов');
-let amount2 = Number(prompt('Во сколько это обойдется?'));
+// let expences2 = prompt('Введите обязательную статью расходов');
+// let amount2 = Number(prompt('Во сколько это обойдется?'));
 
+let expences = [];
 
-
-function getExpensesMonth() { // функции урока 4
-    return amount1 + amount2;
+function getExpensesMonth() { 
+    let sum = 0;
+    for (let i = 0; i != 2; i++) {
+        expences[i] = prompt('Введите обязательную статью расходов');
+        let temp = +prompt('Во сколько это обойдётся?');
+        while (!isNum(temp)) { // проверка на число
+            temp = +prompt('Во сколько это обойдётся? Введите число');
+        }
+        sum += temp;
+    }
+    return sum;
 }
-console.log('getExpensesMonth(): ', getExpensesMonth());
+
+let expencesAmount = getExpensesMonth();
+
+console.log('getExpensesMonth(): ', expencesAmount);
 
 
 function getAccumulatedMonth() {
-    return money - getExpensesMonth();
+    return money - expencesAmount;
 }
 
 
 let accumulatedMonth = getAccumulatedMonth();
 function getTargetMonth() {
-    return Math.ceil(mission / accumulatedMonth);
+    let temp = Math.ceil(mission / accumulatedMonth);
+    if (temp <= 0) {
+        return 'Цель не будет достигнута';
+    }
+    return 'Цель будет достигнута за ' + temp + ' месяцев';
 }
-console.log('getTargetMonth(): ', getTargetMonth());
+
+console.log(getTargetMonth());
 
 
 
