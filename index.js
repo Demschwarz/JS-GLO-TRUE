e8e98191f36ebb1bfdf933f8066c13dfc53c43df
Ilya <chernyi.ilya.00@gmail.com>
2020-04-09 11:29:24 +0300

    lesson 10 probably done laptop

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-'use strict';
-let startId = document.getElementById('start');
-let buttonPlusFirst = document.getElementsByTagName('button')[0];
-let buttonPlusSecond = document.getElementsByTagName('button')[1];
-let buttonStart = document.getElementsByTagName('button')[2];
-let buttonCansel = document.getElementsByTagName('button')[3];
-let checkbox = document.querySelector('#deposit-check')
-let additionalIncomeFirst = document.querySelectorAll('.additional_income-item')[0];
-let additionalIncomeSecond = document.querySelectorAll('.additional_income-item')[1];
-let inputFirst = document.querySelectorAll('.result .budget_day-value');
-let inputSecond = document.querySelectorAll('.result .expenses_month-value');
-let inputThird = document.querySelectorAll('.result .additional_income-value');
-let inputFourth = document.querySelectorAll('.result .additional_expenses-value');
-let inputFifth = document.querySelectorAll('.result .income_period-value');
-let inputSixth = document.querySelectorAll('.result .target_month-value');
-let data = document.querySelector('.data');
-let leftInputFirst = data.querySelector('.salary-amount');
-let leftInputSecond = data.querySelector('.income-title');
-let leftInputThird = data.querySelector('.income-amount');
-let leftInputFourth = data.querySelector('.expenses-title');
-let leftInputFifth = data.querySelector('.expenses-amount');
-let leftInputSixth = data.querySelector('.additional_expenses-item');
-let leftInputSeventh = data.querySelector('.deposit-check');
-let leftInputEighth = data.querySelector('.deposit-amount');
-let leftInputNineth = data.querySelector('.deposit-percent');
-let leftInputTenth = data.querySelector('.target-amount');
-let leftInputEleventh = data.querySelector('.period-select');
+let bookList = document.querySelectorAll('.book'); // пункт 1
+bookList[0].remove();
+bookList[1].remove();
+bookList[2].remove();
+bookList[3].remove();
+bookList[4].remove();
+bookList[5].remove();
+document.querySelector('.books').append(bookList[1]);
+document.querySelector('.books').append(bookList[0]);
+document.querySelector('.books').append(bookList[4]);
+document.querySelector('.books').append(bookList[3]);
+document.querySelector('.books').append(bookList[5]);
+document.querySelector('.books').append(bookList[2]); // пункт 1 закончился
+document.body.style = 'background-image: url(./image/you-dont-know-js.jpg);'; // пункт 2
+document.querySelectorAll('.book')[2].querySelector('a').textContent = 'Книга 3. this и Прототипы Объектов'; // пункт 3
+document.querySelector('.adv').remove(); // пункт 4
+let chapterList = document.querySelectorAll('.book')[4].querySelectorAll('li'); // пункт 5 начало
+chapterList[0].remove();
+chapterList[1].remove();
+chapterList[2].remove();
+chapterList[3].remove();
+chapterList[4].remove();
+chapterList[5].remove();
+chapterList[6].remove();
+chapterList[7].remove();
+chapterList[8].remove();
+chapterList[9].remove();
+chapterList[10].remove();
+let fifthBook = document.querySelectorAll('.book')[4];
+fifthBook.append(chapterList[1]);
+fifthBook.append(chapterList[0]);
+fifthBook.append(chapterList[9]);
+fifthBook.append(chapterList[3]);
+fifthBook.append(chapterList[4]);
+fifthBook.append(chapterList[2]);
+fifthBook.append(chapterList[6]);
+fifthBook.append(chapterList[7]);
+fifthBook.append(chapterList[5]);
+fifthBook.append(chapterList[8]);
+fifthBook.append(chapterList[10]);// пункт 5 конец
+let newElem = document.createElement('li'); // пункт 6 начало
+newElem.textContent = 'Глава 8: За пределами ES6'
+document.querySelectorAll('.book')[5].querySelectorAll('li')[8].after(newElem);
